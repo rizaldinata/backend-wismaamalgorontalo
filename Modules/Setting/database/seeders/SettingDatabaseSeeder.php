6fb0efec414e6237399a2f97fd0995955e2828cc
Rizal Dinata <rizalldnt@gmail.com>
2026-03-07 20:57:20 +0700

    Feat(settings): add settigns migration & model

---
+++ Modules/Setting/database/seeders/SettingDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Setting\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class SettingDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
