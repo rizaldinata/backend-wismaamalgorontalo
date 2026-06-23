292e9bcd1aa4c704637b7ef7f4632d521455c214
rsyidatr <rasyidaturrahma@gmail.com>
2026-06-23 22:54:26 +0700

    fixing alur

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$req = Request::create('/api/admin/guests', 'GET');
-$req->headers->set('Accept', 'application/json');
-$req->setUserResolver(function() { return App\Models\User::first(); });
-$response = app()->handle($req);
-echo $response->getContent();
